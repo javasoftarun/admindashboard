135d9061c0b0a6a33af747987fec82f63e29939b
javasoftarun <javasoftarun159@gmail.com>
2025-05-25 20:50:16 +0530

    changes service details

--- src/components/config/apiConfig.js
+++ src/components/config/apiConfig.js
@@ -1,6 +1,6 @@
-const API_USER_BASE_URL = "https://userservice-zwy2.onrender.com/api";
-const API_CABANDBOOKING_BASE_URL = "https://carbookingservice-4ydj.onrender.com/api";
-const API_COMMONSERVICE_BASE_URL = "https://commonservice-wxam.onrender.com/api";
+const API_USER_BASE_URL = "https://userservice-f6nd.onrender.com/api";
+const API_CABANDBOOKING_BASE_URL = "https://carbookingservice-0mby.onrender.com/api";
+const API_COMMONSERVICE_BASE_URL = "https://commonservice-31h4.onrender.com/api";
 
 const API_ENDPOINTS = {
   // users api endpoints
